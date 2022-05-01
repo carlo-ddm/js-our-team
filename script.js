88834f5bfdb199277e885bb8cf0b45eacb547bef
carlo-ddm <carloduminuco90@gmail.com>
2022-05-01 15:00:21 +0200

    esercizio completo

--- script.js
+++ script.js
@@ -9,33 +9,76 @@ console.log('Test');
 
 const team = [
   {
-    img: 'our-team-base/img/wayne-barnett-founder-ceo.jpg',
+    img: '../our-team-base/img/wayne-barnett-founder-ceo.jpg',
     name: 'Wayne Barnett',
     role: 'Founder & CEO'
   },
   {
-    img: 'our-team-base/img/angela-caroll-chief-editor.jpg',
+    img: '../our-team-base/img/angela-caroll-chief-editor.jpg',
     name: 'Angela Caroll',
     role: 'Chief Editor'
   },
   {
-    img: 'our-team-base/img/walter-gordon-office-manager.jpg',
+    img: '../our-team-base/img/walter-gordon-office-manager.jpg',
     name: 'Walter Gordon',
     role: 'Office Manager'
   },
   {
-    img: 'our-team-base/img/angela-lopez-social-media-manager.jpg',
+    img: '../our-team-base/img/angela-lopez-social-media-manager.jpg',
     name: 'Angela Lopez',
     role: 'Social Media Manager'
   },
   {
-    img: 'our-team-base/img/scott-estrada-developer.jpg',
+    img: '../our-team-base/img/scott-estrada-developer.jpg',
     name: 'Scott Estrada',
     role: 'Developer'
   },
   {
-    img: 'our-team-base/img/barbara-ramos-graphic-designer.jpg',
+    img: '../our-team-base/img/barbara-ramos-graphic-designer.jpg',
     name: 'Barbara Ramos',
     role: 'Graphic Designer'
   }
 ];
+
+console.log('----------');
+
+const teamContainer = document.querySelector('.team-container');
+
+
+console.log(cicle());
+
+cicle();
+
+function cicle() {
+  teamContainer.innerHTML = '';
+
+  for (let membro of team){
+    console.log(team[membro]);
+    print(membro);
+
+  };
+};
+
+
+function print(membro) {
+  let contenuto = teamContainer.innerHTML;
+
+  const {name, role, img} = membro;
+
+  contenuto += `
+  <div class="team-card">
+    <div class="card-image">
+    <img
+      src="img/${img}"
+      alt="Wayne Barnett"
+    />
+  </div>
+  <div class="card-text">
+    <h3>${name}</h3>
+    <p>${role}</p>
+  </div>
+</div>
+`;
+teamContainer.innerHTML = contenuto;
+}
+
